029626d55e1416ec92b1ac0c7cee0731e9bec2a8
Abhinav Goyal <abhinav.goyal@live.com>
2017-08-04 14:07:11 +0700

    testing some stuff

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-var express = require("express");
-let request = require("request");
-let bodyParser = require("body-parser");
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
 
-let app = express();
+const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+
+//listening on heroku
 app.listen((process.env.PORT || 5000));
 
 //home
 app.get("/", (req, res) => {
-	res.send(res.body);
+	res.send("it working 🔥..");
 });
 
 //facebook webhook verification
@@ -23,14 +25,19 @@ app.get("/webhook", (req, res) => {
 		res.sendStatus(403);
 	}
 });
+
 /*
 app.post("/webhook", (req, res) => {
-	if(req.query.object === "page") {
+	console.log(JSON.stringify(req.body));
+	if(req.body.object === "page") {
 
 		//Iterate over each entry - there may be multiple if batched
-		req.query.forEach(entry => {
+		req.body.entry.forEach(entry => {
 			let pageID = entry.id;
+			let timeOfEvent = entry.time;
+			entry.messaging.forEach(event => {
 
+			})
 		})
 	}
 })
